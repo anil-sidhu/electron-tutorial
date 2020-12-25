39979ec1558ca782eb1d9044437fec165d87c5a0
anil sidhu <webanilsidhu@gmail.com>
2020-12-26 00:42:16 +0530

    done app

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ function createWindow() {
         height: 600
     })
     win.loadFile('index.html')
+    let wc=win.webContents;
+    wc.on("dom-ready",()=>{
+        console.warn("app is ready now")
+    })
+    wc.on("did-finish-load",()=>{
+        console.warn("app loading is finished")
+    })
+    wc.on("new-window",()=>{
+        console.warn("new window is open")
+    })
+    wc.on("before-input-event",(x)=>{
+        console.warn("some input event is called ")
+    })
 }
 
 app.whenReady().then(createWindow)
