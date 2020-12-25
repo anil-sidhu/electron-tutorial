63c5a4e7b8b7f6cbb85553e46ea6a1cdb05866ca
anil sidhu <webanilsidhu@gmail.com>
2020-12-25 22:45:02 +0530

    done window web

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const { app, BrowserWindow } = require('electron')
-
+const windowStateKeeper= require('electron-window-state')
+let win;
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 900,
-        height: 600
+    const mainWindowState= windowStateKeeper({
+        defaultHeight:900,
+        width:600
+    })  
+     win = new BrowserWindow({ 
+         x:mainWindowState.x,
+         y:mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height
     })
     win.loadFile('index.html')
+    mainWindowState.manage(win)
 }
-
-app.whenReady().then(createWindow)
+ 
+app.whenReady().then(createWindow) 
