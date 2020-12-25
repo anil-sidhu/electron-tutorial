edb42e643033265b6730c2e2a9c134cbac537efc
anil sidhu <webanilsidhu@gmail.com>
2020-12-25 16:23:35 +0530

    done code

--- main.js
+++ main.js
@@ -7,5 +7,26 @@ function createWindow() {
     })
     win.loadFile('index.html')
 }
+// setTimeout(() => {
+//     console.warn(app.isReady())
+// }, 1000);
+// app.whenReady().then(createWindow)
+// app.on("before-quit",(e)=>{
+//     console.warn("called before app quit")
+//     e.preventDefault();
+// })
+// app.on("will-quit",(e)=>{
+//     console.warn("called will app quit")
+//     e.preventDefault();
+// })
 
-app.whenReady().then(createWindow)
+app.on('browser-window-focus',()=>{
+    console.warn("you are currently using this app");
+})
+app.on('browser-window-blur',()=>{
+    console.warn("you are  not using this app");
+})
+app.on('ready',()=>{
+   
+    createWindow();
+})
