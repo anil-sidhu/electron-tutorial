38bd18643e757a62a87a89b7198344f8857cd662
anil sidhu <webanilsidhu@gmail.com>
2020-12-25 18:21:03 +0530

    done

--- main.js
+++ main.js
@@ -3,30 +3,13 @@ const { app, BrowserWindow } = require('electron')
 function createWindow() {
     const win = new BrowserWindow({
         width: 900,
-        height: 600
+        height: 600,
+        frame:false
     })
     win.loadFile('index.html')
 }
-// setTimeout(() => {
-//     console.warn(app.isReady())
-// }, 1000);
-// app.whenReady().then(createWindow)
-// app.on("before-quit",(e)=>{
-//     console.warn("called before app quit")
-//     e.preventDefault();
-// })
-// app.on("will-quit",(e)=>{
-//     console.warn("called will app quit")
-//     e.preventDefault();
-// })
-
-app.on('browser-window-focus',()=>{
-    console.warn("you are currently using this app");
-})
-app.on('browser-window-blur',()=>{
-    console.warn("you are  not using this app");
-})
 app.on('ready',()=>{
    
     createWindow();
 })
+ 
