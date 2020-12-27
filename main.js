86cbd7f458f1d67eb8f79795905d0f91f1a8aded
anil sidhu <webanilsidhu@gmail.com>
2020-12-27 16:00:53 +0530

    done with context menu

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const { app, BrowserWindow } = require('electron')
-
+const { app, BrowserWindow ,Menu} = require('electron')
+let template=[{label:'item 1'},{label:'item 2'},{role:'quit',label:'Close'},{role:'minimize'}]
+let contextMenu=Menu.buildFromTemplate(template)
 function createWindow() {
     const win = new BrowserWindow({
         width: 900,
         height: 600
     })
     win.loadFile('index.html')
+    win.webContents.on('context-menu',()=>{
+        contextMenu.popup();
+    })
 }
 
 app.whenReady().then(createWindow)
