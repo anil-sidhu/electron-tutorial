43214e392dcc0205f911b159b8e6eb67d99971c8
anil sidhu <webanilsidhu@gmail.com>
2020-12-27 23:14:29 +0530

    done with tray

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,Tray,Menu } = require('electron')
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -6,6 +6,15 @@ function createWindow() {
         height: 600
     })
     win.loadFile('index.html')
+    tray = new Tray('orbit.png')
+    tray.setToolTip('Tray for electron js')
+    // tray.on("click",()=>{
+    //     win.isVisible()?win.hide():win.show()
+    // })
+    let template =[{label:'item 1'},{label:'item 2',type:"radio"}]
+    let contextMenu=Menu.buildFromTemplate(template)
+    tray.setContextMenu(contextMenu)
+    
 }
 
 app.whenReady().then(createWindow)
