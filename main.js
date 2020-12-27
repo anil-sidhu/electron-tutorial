e3143899b524a84328e468aec69fcabd2a0d568e
anil sidhu <webanilsidhu@gmail.com>
2020-12-27 13:25:40 +0530

    done menu

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,Menu } = require('electron')
 
+let isMac=process.platform=="darwin";
+let template=[
+   ...isMac? {label:'Blog',submenu:[
+        {label:'About'},
+        {label:'Help'}
+    ]}:[],
+    {label:'File'},
+    {label:'Operation',submenu:[
+       isMac? {role:'close',label:'Quit'}: {role:'quit',label:'Quit'},
+        {label:'Zoom'}
+    ]}, 
+
+]
+let menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 function createWindow() {
     const win = new BrowserWindow({
         width: 900,
